815eac5a3a4667f373130a9800f490c72596dfde
rikutokd <rikutokd@yahoo.co.jp>
2023-01-18 23:53:07 +0900

    [update]join,leave,speaking状態を取得可

--- index.js
+++ index.js
@@ -1,21 +1,83 @@
 const { Client, GatewayIntentBits } = require('discord.js')
-require('dotenv/config')
 
 const client = new Client ({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates
   ],
 })
+
+require('dotenv/config')
+
+const { joinVoiceChannel, entersState, VoiceConnectionStatus, createAudioResource, StreamType, createAudioPlayer, AudioPlayerStatus, NoSubscriberBehavior, generateDependencyReport, getVoiceConnection } = require("@discordjs/voice");
+
+const recorder = require('node-record-lpcm16');
+
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'ja-JP';
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: false, // If you want interim results, set this to true
+};
+
 
 client.on('ready', () => {
   console.log('Sukoxa is ready')
 })
 
+client.on('messageCreate', async message => {
+  if(message.content === '!sjoin') {
+    const guild = message.guild
+    const vc = message.member.voice.channel;
+
+    if(!vc) {
+      return message.reply('ボイスチャンネルに参加してね');
+    }
+
+    const connection = joinVoiceChannel({
+      guildId: guild.id,
+      channelId: vc.id,
+      adapterCreator: guild.voiceAdapterCreator,
+      selfMute: false,
+      selfDeaf: false,
+    });
+
+    connection.receiver.speaking.on('start', (userId) => {
+        console.log(userId + 'is speaking');
+    })
+
+  }
+})
+
 client.on('messageCreate', message => {
   if(message.content === '!js') {
     message.reply('jsだよ')
+  }
+})
+
+client.on('messageCreate', message => {
+  if(message.content === '!sleave') {
+    const vc = message.member.voice.channel;
+
+    if(!vc) {
+      return message.reply('ボイスチャンネルに参加してないよ');
+    }
+
+    getVoiceConnection(message.guild.id).destroy();
   }
 })
 
