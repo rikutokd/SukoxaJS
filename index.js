bdd7fcca79c174827f257de1a8d668f6e727ec0d
rikutokd <rikutokd@yahoo.co.jp>
2023-01-20 02:36:47 +0900

    [update]speech to text 追加

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const recorder = require('node-record-lpcm16');
 // Imports the Google Cloud client library
 const speech = require('@google-cloud/speech');
 
+const speechClient = new speech.SpeechClient();
+
 /**
  * TODO(developer): Uncomment the following lines before running the sample.
  */
@@ -56,8 +58,39 @@ client.on('messageCreate', async message => {
       selfDeaf: false,
     });
 
-    connection.receiver.speaking.on('start', (userId) => {
-        console.log(userId + 'is speaking');
+    const receiver = connection.receiver;
+
+    receiver.speaking.on('start', (userId) => {
+        console.log('user : ' + userId + ' is speaking');
+        
+        // Create a recognize stream
+        const recognizeStream = speechClient
+          .streamingRecognize(request)
+          .on('error', console.error)
+          .on('data', data =>
+            process.stdout.write(
+              data.results[0] && data.results[0].alternatives[0]
+                ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+                : '\n\nReached transcription time limit, press Ctrl+C\n'
+            )
+          );
+
+        // Start recording and send the microphone input to the Speech API.
+        // Ensure SoX is installed, see https://www.npmjs.com/package/node-record-lpcm16#dependencies
+        recorder
+          .record({
+            sampleRateHertz: sampleRateHertz,
+            threshold: 0,
+            // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+            verbose: false,
+            recordProgram: 'rec', // Try also "arecord" or "sox"
+            silence: '10.0',
+          })
+          .stream()
+          .on('error', console.error)
+          .pipe(recognizeStream);
+
+          console.log('Listening, press Ctrl+C to stop.');
     })
 
   }
