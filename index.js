00da397eb96d6a5e414bbfd960cedab7d777ec77
rikutokd <rikutokd@yahoo.co.jp>
2023-01-22 16:28:06 +0900

    [update]特定のワードのみに反応して検索

--- index.js
+++ index.js
@@ -90,60 +90,73 @@ client.on('messageCreate', async message => {
 
               console.dir(stdoutText, { depth: null });
 
-              const suffix = ['かけて','流して','聞かせて'];
+              const suffix = ['かけて','流して','聞かせて','ながして'];
+
+              var array = [];
 
               for (let i = 0; i < suffix.length; i++) {
+                array.push(stdoutText.match(suffix[i]));
                 stdoutText = stdoutText.replace(suffix[i],'');
               }
-            
-              console.log(stdoutText)
 
-              youtube.setKey(process.env.ytAPIKey);
-
-              youtube.addParam('order', 'viewCount');
-              youtube.addParam('type', 'video');
-              youtube.addParam('regionCode', 'JP');
-
-              const urls = [];
-
-              youtube.search(stdoutText, limit, function(err, result) {
-                if (err) { console.log(err); return; }
-                items = result["items"];
-                for (var i in items) {
-                    item = items[i];
-                    title = item["snippet"]["title"];
-                    id = item["id"]["videoId"];
-                    URL = "https://www.youtube.com/watch?v=" + id;
-    
-                    console.log("title : " + title);
-                    console.log("URL : " + URL);
-                    console.log("-------------------------------");
-
-                    urls.push(URL);
+              var result = false;
+              array.forEach(function(element){
+                if(element != null){
+                  console.log('検索します')
+                  result = true;
                 }
+              });
 
-                console.log(urls[0])
+              if (result == true) {
+                console.log('検索値 : ' + stdoutText);
 
-                const player = createAudioPlayer();
-                connection.subscribe(player);
-  
-                const stream = ytdl(ytdl.getURLVideoID(urls[0]), {
-                  filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm opus
-                  quality: 'highest',
-                  highWaterMark: 32 * 1024 * 1024, // https://github.com/fent/node-ytdl-core/issues/902
-                });
-  
-                const resource = createAudioResource(stream, {
-                  inputType: StreamType.WebmOpus
-                });
-  
-                // 再生
-                player.play(resource);
+                youtube.setKey(process.env.ytAPIKey);
 
-              })
+                youtube.addParam('order', 'viewCount');
+                youtube.addParam('type', 'video');
+                youtube.addParam('regionCode', 'JP');
 
+                const urls = [];
+
+                youtube.search(stdoutText, limit, function(err, result) {
+                  if (err) { console.log(err); return; }
+                  items = result["items"];
+                  for (var i in items) {
+                      item = items[i];
+                      title = item["snippet"]["title"];
+                      id = item["id"]["videoId"];
+                      URL = "https://www.youtube.com/watch?v=" + id;
+      
+                      console.log("title : " + title);
+                      console.log("URL : " + URL);
+                      console.log("-------------------------------");
+
+                      urls.push(URL);
+                  }
+
+                  console.log(urls[0])
+
+                  const player = createAudioPlayer();
+                  connection.subscribe(player);
+    
+                  const stream = ytdl(ytdl.getURLVideoID(urls[0]), {
+                    filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm opus
+                    quality: 'highest',
+                    highWaterMark: 32 * 1024 * 1024, // https://github.com/fent/node-ytdl-core/issues/902
+                  });
+    
+                  const resource = createAudioResource(stream, {
+                    inputType: StreamType.WebmOpus
+                  });
+    
+                  // 再生
+                  player.play(resource);
+                  result = null;
+                })
+              }else if(result == false){
+                console.log('検索しません')
+              }
             }
-
           });
 
         // Start recording and send the microphone input to the Speech API.
