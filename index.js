2117da9a1a9366fc6b8cfad83baf90519a6b12ee
rikutokd <rikutokd@yahoo.co.jp>
2023-01-29 02:44:57 +0900

    fix

--- index.js
+++ index.js
@@ -281,7 +281,7 @@ function startRecognizeStream(guild,connection) {
       threshold: 0,
       // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
       verbose: false,
-      recordProgram: 'rec', // Try also "arecord" or "sox"
+      recordProgram: 'sox', // Try also "arecord" or "sox"
       silence: '10.0',
     })
     .stream()
