a5daaa96c1522e4fec22c46b3dfdc8b70ecf14f5
rikutokd <rikutokd@yahoo.co.jp>
2023-02-05 17:47:49 +0900

    [update]音声認識をローカルファイルで実行する用に変更

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 const prism = require('prism-media');
+const { exec } = require('child_process')
+
 
 const client = new Client ({
   intents: [
@@ -29,8 +31,6 @@ let isPlaying = false;
 
 const { joinVoiceChannel, createAudioResource, StreamType, createAudioPlayer, AudioPlayerStatus, getVoiceConnection, EndBehaviorType} = require("@discordjs/voice");
 
-const recorder = require('node-record-lpcm16');
-
 // Imports the Google Cloud client library
 const speech = require('@google-cloud/speech');
 
@@ -39,8 +39,8 @@ const speechClient = new speech.SpeechClient();
 /**
  * TODO(developer): Uncomment the following lines before running the sample.
  */
-const encoding = 'LINEAR16';
-const sampleRateHertz = 16000;
+const encoding = 'FLAC';
+const sampleRateHertz = 44100;
 const languageCode = 'ja-JP';
 
 
@@ -48,7 +48,7 @@ client.on('ready', () => {
   console.log('Sukoxa is ready')
 })
 
-client.on('messageCreate', async message => {
+client.on('messageCreate', message => {
   if(message.content === '!sjoin') {
     const guild = message.guild
     const vc = message.member.voice.channel;
@@ -69,48 +69,9 @@ client.on('messageCreate', async message => {
 
     const receiver = connection.receiver;
 
-    receiver.speaking.on('start', async (userId) => {
-      const voiceStream = receiver.subscribe(userId, { 
-        end: {
-          behavior: EndBehaviorType.AfterSilence,
-          duration: 1000,
-        }
-      })
-
-      let decoder = new prism.opus.Decoder({ 
-        channels: 2, 
-        rate: 48000, 
-        frameSize: 960 
-      });
+    receiver.speaking.on('start', (userId) => {
       
-      const filename = `./recordings/${Date.now()}-${userId}.pcm`;
-
-      console.log(`👂 Started recording ${filename}`);
-      const writer = voiceStream.pipe(decoder).pipe(fs.createWriteStream(`${filename}`));
-
-      writer.on("finish", () => {
-        const content = fs.readFileSync(filename).toString('base64');
-
-        const request = {
-          config: {
-            encoding: encoding,
-            sampleRateHertz: sampleRateHertz,
-            languageCode: languageCode,
-          },
-          interimResults: false, // If you want interim results, set this to true
-          audio: {
-            "content": content,
-          }
-        };
-      
-        // Detects speech in the audio file
-        const response = speechClient.recognize(request);
-        const transcription = response.results
-
-        console.log('Transcription: ', transcription);
-      })
-
-      //startRecognizeStream(guild, connection, filename);
+      startRecognizeStream(guild, connection, userId);
     })
 
   }
@@ -119,10 +80,10 @@ client.on('messageCreate', async message => {
 client.on('messageCreate', message => {
   if(message.content === '!shelp') {
     message.reply('!sjoin : ボイスチャンネルに参加して再生したい曲のタイトルを聞く状態になるよ。「誰誰の△△」だと尚、流したい曲が流せる' + "\n"
-    + '！注意 音楽再生中は音声認識が止まっているよ' + "\n"
-    + '曲を止めるか再生が終わると自動でタイトルを聞く状態になるよ' + "\n"
-    + 'ボイスチャンネルから蹴る → !sleave or 「(スコッサ) ディスコネクト」って言う' + "\n"
-    + '曲を止めたい時 → !stop'
+    + '⚠️ 音楽再生中は音声認識が止まっているよ ⚠️' + "\n"
+    + '曲を止めるか再生が終わると自動でタイトルを聞く状態になるよ' + "\n" + "\n"
+    + 'ボイスチャンネルから蹴る: !sleave or 「(スコッサ) ディスコネクト」って言う' + "\n"
+    + '曲を止めたい時: !stop'
     )
   }
 })
@@ -169,7 +130,7 @@ client.on('messageCreate', message => {
     const receiver = connection.receiver;
 
     receiver.speaking.on('start', (userId) => {
-      startRecognizeStream(guild, connection, filename);
+      startRecognizeStream(guild, connection, userId);
     })
 
   }
@@ -180,103 +141,161 @@ client.login(process.env.TOKEN)
 
 setInterval(() => {
   console.log('isPlaying : ' + isPlaying);
-}, 10000);
+}, 20000);
 
-async function startRecognizeStream(guild,connection, filename) {
+// exit時
+process.on("exit", function() {
+  exec('npm start')
+});
+
+
+async function startRecognizeStream(guild, connection, userId) {
   if(isPlaying == true) {
       return
     }
 
-    // if (data.results[0] && data.results[0].alternatives[0]) {
-    //   //音声認識が継続した場合を考慮して、最新の認識結果のみを取得
-    //   let i = data.results.length;
-    //   let stdoutText = data.results[i].alternatives[0].transcript;
+  const receiver = connection.receiver;
 
-    //   console.dir(stdoutText, { depth: null });
+  await fs.promises.mkdir('./recordings', { recursive: true })
 
-    //   var pattern = new RegExp('ディスコネクト');
-    //   let doDisconnect = pattern.test(stdoutText);
+    const voiceStream = receiver.subscribe(userId, { 
+      end: {
+        behavior: EndBehaviorType.AfterSilence,
+        duration: 1000,
+      }
+    })
+    
+    const decoder = new prism.opus.Decoder({ rate: 48000, channels: 1, frameSize: 960 });
 
-    //   if (doDisconnect == true) {
-    //     console.log('disconnectします')
-    //     getVoiceConnection(guild.id).destroy();
-    //   }
+    const filename = `./recordings/${Date.now()}-${userId}.pcm`;
 
-    //   const suffix = ['かけて','流して','聞かせて','ながして'];
+    console.log(`👂 Started recording ${filename}`);
+    const writer = voiceStream
+    .pipe(decoder)
+    .pipe(fs.createWriteStream(`${filename}`))
 
-    //   var array = [];
+    writer.on("finish", () => {
+      const ffmpeg = exec(`ffmpeg -f s16le -ar 44.1k -ac 1 -i ${filename} ./recordings/output.flac`)
 
-    //   for (let i = 0; i < suffix.length; i++) {
-    //     var pattern = new RegExp(suffix[i]);
-    //     array.push(pattern.test(stdoutText));
-    //     stdoutText = stdoutText.replace(suffix[i],'');
-    //   }
+      const request = {
+        config: {
+          encoding: encoding,
+          sampleRateHertz: sampleRateHertz,
+          languageCode: languageCode,
+        },
+        interimResults: false, // If you want interim results, set this to true
+      };
+    
+      const recognizeStream = speechClient
+      .streamingRecognize(request)
+      .on('error', console.error)
+      .on('data', data => {
+        console.log(
+          `Transcription: ${data.results[0].alternatives[0].transcript}`
+        );
 
-    //   var result = false;
-    //   array.forEach(function(element){
-    //     if(element == true){
-    //       console.log('検索します')
-    //       result = true;
-    //     }
-    //   });
+        if (data.results[0] && data.results[0].alternatives[0]) {
+          let stdoutText = data.results[0].alternatives[0].transcript;
+    
+          console.dir(stdoutText, { depth: null });
+    
+          var pattern = new RegExp('ディスコネクト');
+          let doDisconnect = pattern.test(stdoutText);
+    
+          if (doDisconnect == true) {
+            console.log('disconnectします')
+            getVoiceConnection(guild.id).destroy();
+          }
+    
+          const suffix = ['かけて','流して','聞かせて','ながして'];
+    
+          var array = [];
+    
+          for (let i = 0; i < suffix.length; i++) {
+            var pattern = new RegExp(suffix[i]);
+            array.push(pattern.test(stdoutText));
+            stdoutText = stdoutText.replace(suffix[i],'');
+          }
+    
+          var result = false;
+          array.forEach(function(element){
+            if(element == true){
+              console.log('検索します')
+              result = true;
+            }
+          });
+    
+          if (result == true) {
+            recognizeStream.end();
+            console.log('検索値 : ' + stdoutText);
+    
+            var youtube = new Youtube();
+            youtube.setKey(process.env.ytAPIKey);
+    
+            youtube.addParam('order', 'viewCount');
+            youtube.addParam('type', 'video');
+            youtube.addParam('regionCode', 'JP');
+    
+            youtube.search(stdoutText, limit, function(err, result) {
+              if (err) { console.log(err); return; }
+              items = result["items"];
+              for (var i in items) {
+                  item = items[i];
+                  title = item["snippet"]["title"];
+                  id = item["id"]["videoId"];
+                  URL = "https://www.youtube.com/watch?v=" + id;
+    
+                  console.log("title : " + title);
+                  console.log("URL : " + URL);
+                  console.log("-------------------------------");
+              }
+    
+              if (!URL) {
+                console.log('検索がヒットしませんでした')
+                return
+              }
+    
+              const player = createAudioPlayer();     
+              connection.subscribe(player);
+    
+              const stream = ytdl(ytdl.getURLVideoID(URL), {
+                filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm opus
+                quality: 'highest',
+                highWaterMark: 32 * 1024 * 1024, // https://github.com/fent/node-ytdl-core/issues/902
+              });
+    
+              const resource = createAudioResource(stream, {
+                inputType: StreamType.WebmOpus
+              });
+    
+              // 再生
+              player.play(resource);
+              player.on(AudioPlayerStatus.Playing, () => {
+                console.log('Sukoxa has started playing!');
+                isPlaying = true;
+              });
+              player.on(AudioPlayerStatus.Idle, () => {
+                console.log('Sukoxa is idle.');
+                isPlaying = false;
+              });
+            })
+          }else if(result == false){
+            console.log('検索しません')
+          }
+        }
 
-    //   if (result == true) {
-    //     recognizeStream.end();
-    //     console.log('検索値 : ' + stdoutText);
+      });
 
-    //     var youtube = new Youtube();
-    //     youtube.setKey(process.env.ytAPIKey);
-
-    //     youtube.addParam('order', 'viewCount');
-    //     youtube.addParam('type', 'video');
-    //     youtube.addParam('regionCode', 'JP');
-
-    //     youtube.search(stdoutText, limit, function(err, result) {
-    //       if (err) { console.log(err); return; }
-    //       items = result["items"];
-    //       for (var i in items) {
-    //           item = items[i];
-    //           title = item["snippet"]["title"];
-    //           id = item["id"]["videoId"];
-    //           URL = "https://www.youtube.com/watch?v=" + id;
-
-    //           console.log("title : " + title);
-    //           console.log("URL : " + URL);
-    //           console.log("-------------------------------");
-    //       }
-
-    //       if (!URL) {
-    //         console.log('検索がヒットしませんでした')
-    //         return
-    //       }
-
-    //       const player = createAudioPlayer();     
-    //       connection.subscribe(player);
-
-    //       const stream = ytdl(ytdl.getURLVideoID(URL), {
-    //         filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm opus
-    //         quality: 'highest',
-    //         highWaterMark: 32 * 1024 * 1024, // https://github.com/fent/node-ytdl-core/issues/902
-    //       });
-
-    //       const resource = createAudioResource(stream, {
-    //         inputType: StreamType.WebmOpus
-    //       });
-
-    //       // 再生
-    //       player.play(resource);
-    //       player.on(AudioPlayerStatus.Playing, () => {
-    //         console.log('Sukoxa has started playing!');
-    //         isPlaying = true;
-    //       });
-    //       player.on(AudioPlayerStatus.Idle, () => {
-    //         console.log('Sukoxa is idle.');
-    //         isPlaying = false;
-    //       });
-    //     })
-    //   }else if(result == false){
-    //     console.log('検索しません')
-    //   }
-    // }
+      ffmpeg.on("exit", async () => {
+        try {
+          // Stream an audio file from disk to the Speech API, e.g. "./resources/audio.raw"
+          fs.readFileSync('./recordings/output.flac')
+          fs.createReadStream('./recordings/output.flac').pipe(recognizeStream)
+          await fs.promises.rm('./recordings', { recursive: true, force: true })
+        } catch (error) {
+          console.log(error)
+        }
+      })
+    })
   
 }
